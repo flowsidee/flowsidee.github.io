7d52da23384d70f63295c9aa8fa8b360400ff7de
flowside <xenoblad33@gmail.com>
2023-07-06 19:16:45 +0700

    v

--- test.js
+++ test.js
@@ -1,26 +1,2 @@
 let tg = window.Telegram.WebApp;
-
-tg.expand();
-
-tg.MainButton.textColor = "#FFFFF";
-tg.MainButton.color = "#2cab37";
-
-let item = "";
-
-let btn1 = document.getElementByID("btn_nigger");
-
-btn1.addEventListener("click", function() {
-	alert("main button clicked");
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Негр пашет круче всех");
-		item = "nigger";
-		tg.MainButton.show();
-	}
-});
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
+tg.showAlert(`Добро пожаловать, @${tg.WebAppUser.username}.`);
