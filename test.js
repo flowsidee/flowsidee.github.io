30c9149b2c19c61a030e9deff34b0b4eff257a43
flowside <xenoblad33@gmail.com>
2023-07-06 19:35:28 +0700

    м

--- test.js
+++ test.js
@@ -1,23 +1,4 @@
-let tg = window.Telegram.WebApp;
 
-tg.expand();
-
-tg.MainButton.textColor = "#FFFFF";
-tg.MainButton.color = "#2cab37";
-
-let item = "";
-
-let btn1 = document.getElementById("btn1");
-
-btn1.addEventListener("click", function() {
-	tg.MainButton.setText("Вы нег!");
-	item = "1";
-	tg.MainButton.show();
-})
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-})
 
 //let tg = window.Telegram.WebApp;
 //
