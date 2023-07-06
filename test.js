2099cb3419a4905250a0780e9de30d736c67259a
flowside <xenoblad33@gmail.com>
2023-07-06 19:11:16 +0700

    Update test.js

--- test.js
+++ test.js
@@ -10,16 +10,12 @@ let item = "";
 let btn1 = document.getElementByID("btn_nigger");
 
 btn1.addEventListener("click", function() {
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Негр пашет круче всех");
-		item = "nigger";
-		tg.MainButton.show();
-	}
+	tg.MainButton.setText("Негр пашет круче всех");
+	item = "nigger";
+	tg.MainButton.show();
+
 });
 
-tg.onEvent("mainButtonClicked", function(){
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
